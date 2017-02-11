56d01eab1826b2a16d22f431f0bddb2228915ba1
ducbka <trungducphan95@gmail.com>
2017-02-11 10:52:02 +0700

    profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "static_pages#show", page: "home"
+  root "static_pages#index"
   get "static_pages/*page", to: "static_pages#show"
 
   namespace :admin do
