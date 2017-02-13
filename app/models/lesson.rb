7a8492a0fc5d5838e071deaac894ec37ff8f6d5a
dieptran <dieptx.ptit@gmail.com>
2017-02-13 16:55:32 +0700

    User do lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,4 +4,32 @@ class Lesson < ApplicationRecord
 
   has_many :results, dependent: :destroy
   has_many :words, through: :results
+
+  accepts_nested_attributes_for :results
+
+  enum status: {init: 0, finished: 1, in_progress: 2}
+
+  before_create :init_results
+
+  scope :order_desc, -> {order created_at: :desc}
+
+  def set_score
+    score = 0
+    results.each do |result|
+      if result.answer.present? && result.answer.is_correct?
+        score += 1
+      end
+    end
+    assign_attributes score: score
+  end
+
+  def init_results
+    self.words = category.words.order("RANDOM()").limit(category
+      .word_per_lesson)
+  end
+
+  def start_lesson
+    assign_attributes finish_time: Time.now + category.duration.minutes
+    in_progress!
+  end
 end
