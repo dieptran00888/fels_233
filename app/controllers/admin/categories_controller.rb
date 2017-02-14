1f10e3f0fc656d300b48429435c1c08dcb6440c7
dieptran <dieptx.ptit@gmail.com>
2017-02-14 16:27:22 +0700

    Edit and delete category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -20,12 +20,33 @@ class Admin::CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    render layout: false
+  end
+
+  def update
+    if @category.update_attributes category_params
+      flash.now[:success] = t "admin.edit_category_success_message"
+    end
+    respond_to do |format|
+      format.js
+    end
+  end
+
   def show
     @word = Word.new
     Settings.mininum_answers_count.times {@word.answers.build}
     @words = @category.words
   end
-  
+
+  def destroy
+    @category.destroy
+    flash.now[:success] = t "admin.delete_category_success_message"
+    respond_to do |format|
+      format.js
+    end
+  end
+
   private
   def category_params
     params.require(:category).permit :name, :duration, :word_per_lesson
